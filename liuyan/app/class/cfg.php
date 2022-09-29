b74c475337b4822aabbc714da100cf0654f6e25a
xiaowang <102400208+awangx@users.noreply.github.com>
2022-09-29 17:53:26 +0800

    Add files via upload

---
+++ liuyan/app/class/cfg.php
@@ -0,0 +1,11 @@
+
+<?php
+//本源码归许愿数据所有
+error_reporting(0);
+session_start();
+date_default_timezone_set('PRC');
+define('KEY','b46636'); 
+define('BASE_PATH',str_replace('\\','/',dirname(__FILE__))."/");
+define('ROOT_PATH',str_replace('app/class/','',BASE_PATH));
+define('DB',ROOT_PATH.'app/db/f30b15cd27c8.db');
+/*installed*/
